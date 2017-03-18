668e3d788d5eaf8b6f7f68d4cb9addf42a0669c3
Zameer Ansari <xameeramir@users.noreply.github.com>
2017-03-18 12:52:05 +0530

    Added Angular magic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(window).load(function() {
-    $('.leoslider').leoslider({
-      animation: "slide"
-    });
+  $('.leoslider').leoslider({
+    animation: "slide"
+  });
 });
+
+var myApp = angular.module('SAZInteriorsApp', []);
+
+myApp.controller('SAZInteriorsCtrl', ['$scope', function($scope) {
+  $scope.currUrl = window.location.href;
+}]);
